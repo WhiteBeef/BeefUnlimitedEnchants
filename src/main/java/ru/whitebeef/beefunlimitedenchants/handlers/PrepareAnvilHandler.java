f0f2e9606e028a617f174e901c09d878ecc3b6bd
whitebeef <whitebeef.official@gmail.com>
2022-11-09 17:48:31 +0300

    Fixed some bugs

--- src/main/java/ru/whitebeef/beefunlimitedenchants/handlers/PrepareAnvilHandler.java
+++ src/main/java/ru/whitebeef/beefunlimitedenchants/handlers/PrepareAnvilHandler.java
@@ -1,6 +1,10 @@
 package ru.whitebeef.beefunlimitedenchants.handlers;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
@@ -13,30 +17,57 @@ import java.util.Map;
 
 public class PrepareAnvilHandler implements Listener {
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPrepareAnvilRecipe(PrepareAnvilEvent event) {
         AnvilInventory inventory = event.getInventory();
 
-        if (inventory.getFirstItem() == null || inventory.getSecondItem() == null || inventory.getResult() == null) {
+        if (inventory.getFirstItem() == null || inventory.getSecondItem() == null) {
             return;
         }
-
-        ItemStack firstItem = inventory.getFirstItem();
-        ItemStack secondItem = inventory.getSecondItem();
+        ItemStack firstItem = inventory.getFirstItem().clone();
+        ItemStack secondItem = inventory.getSecondItem().clone();
         ItemStack result = inventory.getResult();
         Map<Enchantment, Integer> toSet = new HashMap<>();
 
+        boolean ignoreConflicts = false;
+        ItemMeta meta = null;
+        if (result == null && secondItem.getType() == Material.ENCHANTED_BOOK) {
+            result = firstItem.clone();
+            meta = result.getItemMeta();
+            if (inventory.getRenameText() != null && !inventory.getRenameText().isEmpty()) {
+                meta.displayName(Component.text(inventory.getRenameText()));
+            }
+            for (Enchantment bookEnchantment : secondItem.getEnchantments().keySet()) {
+                boolean conflict = false;
+                for (Enchantment itemEnchantment : firstItem.getEnchantments().keySet()) {
+                    if (!bookEnchantment.conflictsWith(itemEnchantment)) {
+                        conflict = true;
+                    }
+                }
+                if (!conflict) {
+                    result.addUnsafeEnchantment(bookEnchantment, 1);
+                }
+            }
+        }
+        if (result == null) {
+            return;
+        }
         sumEnchants(secondItem, firstItem, result, toSet);
-
         sumEnchants(firstItem, secondItem, result, toSet);
-
-        ItemMeta meta = event.getResult().getItemMeta();
-
+        if (meta == null) {
+            meta = result.getItemMeta();
+        }
         for (var entry : toSet.entrySet()) {
+            if (entry.getValue() >= 6) {
+                inventory.setRepairCost(39);
+            }
             meta.addEnchant(entry.getKey(), entry.getValue(), true);
         }
 
         result.setItemMeta(meta);
+
+        inventory.setResult(result);
+        event.getViewers().forEach(player -> ((Player) player).updateInventory());
         event.setResult(result);
     }
 
