ccefc01f4ce5ac7f0010c958f667aa0d874aeda5
whitebeef <whitebeef.official@gmail.com>
2022-11-15 02:17:10 +0300

    Prepare to commit new version

--- src/main/java/ru/whitebeef/beefunlimitedenchants/enums/MergeType.java
+++ src/main/java/ru/whitebeef/beefunlimitedenchants/enums/MergeType.java
@@ -1,5 +1,8 @@
 package ru.whitebeef.beefunlimitedenchants.enums;
 
 public enum MergeType {
-    ITEM_ON_ITEM, BOOK_ON_ITEM, BOOK_ON_BOOK, NONE;
+    ITEM_ON_ITEM,
+    BOOK_ON_ITEM,
+    BOOK_ON_BOOK,
+    NONE
 }
